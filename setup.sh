f7fc91c0e649ef6c19f9bf37afd345c7bd9cc2a7
Yongbeom-Kim <dernbu@gmail.com>
2023-06-08 23:59:50 +0800

    Add bash file to set up dotfiles

---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Abort if command not found
+assert_command_found() {
+  if ! command -v $1 &> /dev/null
+  then
+    echo "$1 command is not found, aborting"
+    exit 1
+  fi
+}
+
+# Force symlink (delete file if exists)
+force_symlink() {
+  if [ -L $2 ] || [ -f $2 ] ; then
+    echo "Removing $2..."
+    rm $2
+  fi
+
+  echo "Symlinking $1 to $2..."
+  ln -s $1 $2
+}
+
+
+# Set up VS Code
+echo "Setting up VS Code:"
+assert_command_found "code"
+force_symlink "$PWD/vscode/settings.json" "$HOME/.config/Code/User/settings.json"
+
+# Set up vim
+echo "Setting up Vim:"
+assert_command_found "vim"
+force_symlink "$PWD/vim/.vimrc" "$HOME/.vimrc"
