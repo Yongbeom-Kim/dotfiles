c6afaa1aa23199d13fa9ce9ad23743bf0bcdbd8d
Kim Yongbeom <yongbeom.kim@u.nus.edu>
2025-11-06 13:14:27 +0800

    feat: add some macos specific things

--- scripts/run_dotfiles.sh
+++ scripts/run_dotfiles.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-find dotfiles -type f -not -name '*.new' -print0 | while IFS= read -r -d '' f; do
+find dotfiles -type f -not -name '*.new' -not -name '.DS_Store' -print0 | while IFS= read -r -d '' f; do
   chmod +x "$f"
   # echo "Running $f"
   "./$f"
