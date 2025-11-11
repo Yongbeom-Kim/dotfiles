2fea59e96b7ec71d7adf40d1b444ef387164dd94
Kim Yongbeom <dernbu@gmail.com>
2025-11-11 20:29:57 +0800

    feat: npm yarn pnpm rush

---
+++ install-scripts/node.sh
@@ -0,0 +1,7 @@
+curl -fsSL https://fnm.vercel.app/install | bash -s -- --skip-shell
+
+npm install --global yarn
+
+curl -fsSL https://get.pnpm.io/install.sh | sh -
+
+npm install -g @microsoft/rush
